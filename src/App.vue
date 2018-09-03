c193f41c05d149bc9b2b091a3a14c4c51355a63c
Sandy <the_dusky@icloud.com>
2018-09-03 15:16:35 -0400

    vuex / router 2nd commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
 <template>
-    <section class="section">
-        <div class="container">
-            <AppHeader></AppHeader>
-        </div>
-    </section>
+  <div>
+      <AppHeader></AppHeader>
+      <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import AppHeader from './components/AppHeader';
+import AppHeader from "./components/AppHeader";
 
 export default {
-    name: "App",
-    components: {
-        AppHeader
-    }
+  name: "App",
+  components: {
+    AppHeader
+  }
 };
 </script>
 
 <style>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 import Vue from "vue";
-import SuiVue from "semantic-ui-vue";
+import VueRouter from "vue-router";
+import store from "./store";
 import App from "./App";
+import AuthHandler from "./components/AuthHandler";
 
-Vue.use(SuiVue);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: "/oauth2/callback",
+      component: AuthHandler
+    }
+  ]
+});
 
 new Vue({
-    render: h => h(App)
-}).$mount('#app');
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
